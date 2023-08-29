4e67251a2ec0a7c195de43dd1237350b5700974d
Jocelyn Wensloff <jewensloff@gmail.com>
2023-08-29 08:50:15 -0600

    Removed the Loading component from the trailer page because it hit the buttons

--- src/Components/Trailer/Trailer.js
+++ src/Components/Trailer/Trailer.js
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types'
 
-import Loading from '../Loading/Loading';
 function Trailer ({ trailer, displayMovieDetails, backToHomePage }) {
 const { key, movie_id } = trailer
 
@@ -14,7 +13,6 @@ const { key, movie_id } = trailer
       <button className='back-to-movie-details-button'onClick={() => displayMovieDetails(movie_id)} ><FontAwesomeIcon icon={faCircleArrowLeft} /></button>
       <button className='back-to-home-button'onClick={() => backToHomePage()} > <FontAwesomeIcon icon={faHouse} /> </button>
     </div>
-    <Loading />
     <iframe
       width="853"
       height="480"
